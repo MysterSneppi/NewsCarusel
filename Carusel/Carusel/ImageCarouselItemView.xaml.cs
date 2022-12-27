c2e179778e0ce2f0809619e143ef0ab3ece125be
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2022-12-27 22:27:29 +0200

    Add Project

---
+++ Carusel/Carusel/ImageCarouselItemView.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Carusel
+{
+    /// <summary>
+    /// Interaction logic for ImageCarouselItemView.xaml
+    /// </summary>
+    public partial class ImageCarouselItemView : UserControl
+    {
+        public object ImageSource
+        {
+            get { return (object)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+
+        public object Title
+        {
+            get { return (object)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+
+        public object Text
+        {
+            get { return (object)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+
+        public object ImageWidth
+        {
+            get { return (object)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+        public object ImageHeight
+        {
+            get { return (object)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+
+
+
+
+        public object ButtonText
+        {
+            get { return (object)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ButtonText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(object), typeof(ImageCarouselItemView), new PropertyMetadata(0));
+
+
+
+
+
+        public ImageCarouselItemView()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ Carusel/Carusel/NewsCaruselViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Carusel
+{
+    public delegate void NewsCarusel();
+    internal class NewsCaruselViewModel
+    {
+        public event NewsCarusel? Carusel;
+        private ObservableCollection<ImageCarouselItemView> ImageCarouselItems { get; set; }
+        public ObservableCollection<ImageCarouselItemView> ImageCarouselItemsCurrent { get; set; }
+        public ObservableCollection<RadioButton> RadioButtons { get; set; }
+
+        private int CurrentItem = 0;
+
+           
+        public NewsCaruselViewModel()
+        {
+            ImageCarouselItemsCurrent = new ObservableCollection<ImageCarouselItemView>();
+            ImageCarouselItems = new ObservableCollection<ImageCarouselItemView>();
+            RadioButtons = new ObservableCollection<RadioButton>();
+            AddItemsInCarousel();
+
+            ImageCarouselItemsCurrent.Add(ImageCarouselItems[0]);
+            RadioButtons[0].IsChecked = true;
+
+            Carusel += StartCarusel;
+            Carusel?.Invoke();
+        }
+
+
+        public async void StartCarusel()
+        {
+            while (true)
+            {
+                await Task.Delay(3000);
+
+                if (CurrentItem < RadioButtons.Count)
+                {
+                    ImageCarouselItemsCurrent.Clear();
+                    ImageCarouselItemsCurrent.Add(ImageCarouselItems[RadioButtons[CurrentItem].TabIndex]);
+                    RadioButtons[CurrentItem].IsChecked = true;
+                    CurrentItem++;
+                }
+                else
+                {
+                    ImageCarouselItemsCurrent.Clear();
+                    ImageCarouselItemsCurrent.Add(ImageCarouselItems[RadioButtons[0].TabIndex]);
+                    RadioButtons[0].IsChecked = true;
+                }
+            }
+        }
+
+        private void AddItemsInCarousel()
+        {
+            #region AddItems
+            ImageCarouselItems.Add(new ImageCarouselItemView() { ImageSource = "/Images/1.jpg", ButtonText = "BUTTON", Title = "TEXT", Text = "Information" });
+            ImageCarouselItems.Add(new ImageCarouselItemView() { ImageSource = "/Images/2.png", ButtonText = "BUTTON", Title = "TEXT", Text = "Information" });
+            ImageCarouselItems.Add(new ImageCarouselItemView() { ImageSource = "/Images/3.png", ButtonText = "BUTTON", Title = "TEXT", Text = "Information" });
+            ImageCarouselItems.Add(new ImageCarouselItemView() { ImageSource = "/Images/4.png", ButtonText = "BUTTON", Title = "TEXT", Text = "Information" });
+            #endregion
+
+            for (int i = 0; i < ImageCarouselItems.Count; i++)
+            {
+                RadioButton radioButton = new RadioButton() { GroupName = "Items", TabIndex = i, Margin = new Thickness(0, 0, 16, 0), VerticalAlignment = VerticalAlignment.Center };
+                radioButton.Click += Click;
+                radioButton.Checked += Checked;
+                RadioButtons.Add(radioButton);
+            }
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            ImageCarouselItemsCurrent.Clear();
+            ImageCarouselItemsCurrent.Add(ImageCarouselItems[RadioButtons[CurrentItem].TabIndex]);
+            RadioButtons[CurrentItem].IsChecked = true;
+        }
+
+        private void Checked(object sender, RoutedEventArgs e)
+        {
+            var a = sender as RadioButton;
+            CurrentItem = a.TabIndex;
+        }
+
+    }
+}
